e52a746fd974c5ce9544f3cf61abc5e13523c983
Rajaselvi Jeyaram <selvi@dropthought.com>
2025-06-23 15:24:51 -0700

    incremental changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template :  '<app-survey-builder></app-survey-builder>'
+  template :  '<app-survey-builder></app-survey-builder>',
   // templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
 })
